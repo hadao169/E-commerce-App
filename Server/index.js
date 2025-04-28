cc6f350db8f572dd01092f5116ec521bf1c04124
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-04-28 23:17:14 +0300

    Preparation completed

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
