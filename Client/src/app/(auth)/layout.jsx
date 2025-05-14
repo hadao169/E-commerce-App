6b7e21103c06fcf6bb9c06175dbc07511b85a3eb
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-05-14 14:25:22 +0300

    updated: complete signin-signup form

--- Client/src/app/(auth)/layout.jsx
+++ Client/src/app/(auth)/layout.jsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import { SiShopee } from "react-icons/si";
+import Footer from "@/components/layouts/footer/Footer";
 export default function AuthLayout({ children }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      <header className="flex items-center justify-between xl:px-[18%] px-6 pt-3 pb-5 text-[15px] text-orange-600">
+        <div className="flex items-end gap-4">
+          <Link href="/" className="flex gap-2 items-end">
+            <SiShopee className="text-[45px]" />
+            <h1 className="text-[30px]">Shopee</h1>
+          </Link>
+          <p className="text-black text-2xl hidden md:block">Log in</p>
+        </div>
+        <span>Need help?</span>
+      </header>
+      <div className="w-full bg-[#d0011b]"> {children}</div>
+      <Footer />
+    </section>
+  );
 }
--- Client/src/components/Auth/SigninForm.jsx
+++ Client/src/components/Auth/SigninForm.jsx
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+ 
 import Link from "next/link";
 const SignInForm = () => {
   return (
