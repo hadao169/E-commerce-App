5f65ab2ebf43fc794323d09884315526737d4f8b
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-05-30 13:07:27 +0300

    .

--- Client/src/app/(protected)/layout.jsx
+++ Client/src/app/(protected)/layout.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useAuth } from "@/hooks/useAuth";
+import { useAuth } from "@/lib/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
