fa61d4ef481576797b2b8637d447000ae8d8c2b9
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-05-31 00:18:22 +0300

    .

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -1,8 +1,10 @@
 import BannerWrapper from "@/components/layouts/home/banner-wrapper";
+import Headers from "@/components/layouts/header/Header";
 
 const Homepage = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center">
+      <Headers />
       <div className="w-full bg-white z-50">
         <BannerWrapper />
       </div>
