e57d47dfb47bbdb7eeaeb6cb6250b1ab826a1e8d
Dao Thai Ha <daothaiha1692005@gmail.com>
2025-06-14 03:22:11 +0700

    update cors config

--- Server/src/utils/constants.js
+++ Server/src/utils/constants.js
@@ -1,5 +1,5 @@
 // Whitelist domains that is allowed to access the server APIs
 export const WHITELIST_DOMAINS = [
   "http://localhost:3000",
-  "https://e-commerce-website-ebon-ten.vercel.app/",
+  "https://e-commerce-website-ebon-ten.vercel.app",
 ];
