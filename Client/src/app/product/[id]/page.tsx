4361ab3d5361e09fb2676e2bbc7d309ee38fd374
hadao169 <ha6bnnt@gmail.com>
2025-08-13 16:33:41 +0300

    fix: page props

--- Client/src/app/product/[id]/page.tsx
+++ Client/src/app/product/[id]/page.tsx
@@ -1,11 +1,17 @@
 import { notFound } from "next/navigation";
 import ProductDetailPage from "./ProductDetailPage";
 import { getProductDetail } from "@/services/api/product";
+import { use } from "react";
 
 // Next.js App Router page function
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   try {
-    const product = await getProductDetail(params.id);
+    const { id } = await params;
+    const product = await getProductDetail(id);
     return <ProductDetailPage product={product} />;
   } catch (error) {
     console.error("Error fetching product:", error);
