9143d21b1129bca81d673b419f767ce039880b7c
hadao169 <ha6bnnt@gmail.com>
2025-08-20 21:40:32 +0300

    .

--- Client/src/app/product/[id]/page.tsx
+++ Client/src/app/product/[id]/page.tsx
@@ -2,13 +2,16 @@ import { notFound } from "next/navigation";
 import ProductDetailPage from "./ProductDetailPage";
 import { getProductDetail } from "@/services/api/product";
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string }; // plain object
-}) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
   try {
-    const product = await getProductDetail(params.id); // fetch data
+    const { id } = params;
+    const product = await getProductDetail(id); // fetch data
     return <ProductDetailPage product={product} />; // pass resolved data
   } catch (error) {
     console.error(error);
